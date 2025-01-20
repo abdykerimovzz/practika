1ac86c52945d40bf65a66e55bed5dc269f24424f
abdykerimovzz <abdykerimova398@gmail.com>
2025-01-20 15:40:52 +0600

    Add files via upload
    Signed-off-by: abdykerimovzz <abdykerimova398@gmail.com>

---
+++ script.js
@@ -0,0 +1,5 @@
+// Форманы жибергенде билдирүү көрсөтүү
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Форманы жиберүү процессин токтотот
+    alert("Суранычыңыз бизге жетти! Рахмат.");
+});
